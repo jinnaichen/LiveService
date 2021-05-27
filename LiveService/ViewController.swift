5511b3d27ea622d6c6aba196fd339e2fdce2e477
jinnaichen <jinnaichen@huya.com>
2021-05-27 16:29:35 +0800

    Initial Commit

---
+++ LiveService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveService
+//
+//  Created by jinnaichen on 2021/5/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
